d3bc2e99e845797c4448b312edff04f3a348c374
kikasso <2501@bk.ru>
2012-05-05 17:39:21 +0300

    файлы переводов

---
+++ app/languages/en.php
@@ -0,0 +1,7 @@
+
+<?php
+$_LANG = array(
+    'Привет всему миру от :name' => 'Hello world from :name',
+    'Учим говорить Joostina на разных языках' => 'Learning to speak in different languages ??Joostina',
+);
+?>
